8a585fbc1957d69bed210945298afefc9719f36f
Trowhs <trowhs@gmail.com>
2026-03-22 12:32:02 +0100

    added 8 fonctions: 1 - ft_strlen 2 - ft_isalpha 3 - ft_isdigit 4 - ft_isalnum 5 - ft_isascii 6 - ft_isprint 7 - ft_toupper 8 - ft_tolower

---
+++ libft.h
@@ -0,0 +1,13 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int ft_strlen(const char *str);
+int ft_isalpha(char c);
+int ft_isdigit(char c);
+int ft_isalnum(char c);
+int ft_isascii(char c);
+int ft_isprint(char c);
+int ft_toupper(int c);
+int ft_tolower(int c);
+
+#endif
